46acd4e2306b734e56261ec5114edadde8c92faa
Rayhan123456789 <87824990+Rayhan123456789@users.noreply.github.com>
2021-08-21 23:48:33 +0700

    Add files via upload

---
+++ first.js
@@ -0,0 +1,29 @@
+// BE14_Intro
+// soal 1
+// let nama = "hello";
+
+// console.log(nama);
+
+// soal 2
+// var nama = 1
+
+// console.log ('Rayhan');
+
+// soal 3
+// var nama = 1
+
+// console.log ("Halo Rizky");
+
+// soal 4
+// const number = "10";
+// const number1 = "kelas:10";
+// console.log('kelas:10');
+
+// soal 5    
+// const array = [1, '1', {number: 1}]
+
+// console.log(["Rizky", 10, {nama: "Rizky", kelas: 10}]);
+
+// soal 6
+// const objek = {kelas: ["frontend", "backend", "mobile"]}
+// console.log("mobile")
